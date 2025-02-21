a51405fea0dbf9819e2eca722733ec516a8b2ae5
Ariony92 <zkhan@iut.univ-paris8.fr>
2025-02-21 15:53:47 +0100

    Création de packages

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr07/jeuQuizz/App.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr07/jeuQuizz/App.java
@@ -1,13 +1,7 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr07.jeuQuizz;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr07/jeuQuizz/entities/dtos/JoueurDTO.java
@@ -0,0 +1,4 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2025.gr07.jeuQuizz.entities.dtos;
+
+public class JoueurDTO {
+}
